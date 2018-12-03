9a80bfb8847268d7423e962ec633e7066d19813c
Jeff Horton <jeff@goblinoid.co.uk>
2018-12-03 15:18:22 +0000

    Day 3 - Fix fails on no match

--- src/day3.rs
+++ src/day3.rs
@@ -14,17 +14,17 @@ fn parse_claim(claim: &str) -> Option<Claim> {
         static ref CLAIM_MATCHER: Regex = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
     }
 
-    let matches = CLAIM_MATCHER.captures(claim).unwrap();
-
-    Some(
-        Claim {
-            id: matches.get(1).unwrap().as_str().parse().unwrap(),
-            x: matches.get(2).unwrap().as_str().parse().unwrap(),
-            y: matches.get(3).unwrap().as_str().parse().unwrap(),
-            w: matches.get(4).unwrap().as_str().parse().unwrap(),
-            h: matches.get(5).unwrap().as_str().parse().unwrap()
-        }
-    )
+    CLAIM_MATCHER
+        .captures(claim)
+        .map(|matches|
+            Claim {
+                id: matches.get(1).unwrap().as_str().parse().unwrap(),
+                x: matches.get(2).unwrap().as_str().parse().unwrap(),
+                y: matches.get(3).unwrap().as_str().parse().unwrap(),
+                w: matches.get(4).unwrap().as_str().parse().unwrap(),
+                h: matches.get(5).unwrap().as_str().parse().unwrap(),
+            }
+        )
 }
 
 #[cfg(test)]
@@ -36,7 +36,9 @@ mod tests {
         assert_eq!(
             Some(Claim {id: 1, x: 1, y: 3, w: 4, h: 4}),
             parse_claim("#1 @ 1,3: 4x4")
-        )
+        );
+
+        assert_eq!(None, parse_claim(""))
     }
 
 }
